d974e4e4b42fe3293a767a515d91175de9a59073
azmi <azmi.stuff@gmail.com>
2015-07-25 21:42:14 +0800

    authors.py

--- Lesson_2_Data_in_More_Complex_Formats/07-Extracting_Data/authors.py
+++ Lesson_2_Data_in_More_Complex_Formats/07-Extracting_Data/authors.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # Your task here is to extract data from xml on authors of an article
 # and add it to a list, one item for an author.
 # See the provided data structure for the expected format.
 # The tags for first name, surname and email should map directly
 # to the dictionary keys
 import xml.etree.ElementTree as ET
+import os
 
-article_file = "exampleResearchArticle.xml"
+article_dir = "Lesson_2_Data_in_More_Complex_Formats\\07-Extracting_Data"
+article_file = os.path.join(article_dir, "exampleResearchArticle.xml")
 
 
 def get_root(fname):
@@ -18,12 +20,14 @@ def get_authors(root):
     authors = []
     for author in root.findall('./fm/bibl/aug/au'):
         data = {
-                "fnm": None,
-                "snm": None,
-                "email": None
+                "fnm": author.find('fnm').text,
+                "snm": author.find('snm').text,
+                "email": author.find('email').text
         }
-
-        # YOUR CODE HERE
+        #Solution
+        #data["fnm"] = author.find('./fnm').text
+        #data["snm"] = author.find('./snm').text
+        #data["email"] = author.find('./email').text
 
         authors.append(data)
 
